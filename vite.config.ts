98692290f1ef0d07c1fd89f996c11241a75efbd8
zk-phi <zk-phi@users.noreply.github.com>
2023-01-14 00:39:58 +0900

    Add vite-checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import checker from "vite-plugin-checker";
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), checker({ typescript: true })],
 })

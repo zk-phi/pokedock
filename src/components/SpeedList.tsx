61f3c1a5bf88038701b5173585b58ce85256f5d3
zk-phi <zk-phi@users.noreply.github.com>
2023-01-21 19:48:05 +0900

    update

--- src/components/SpeedList.tsx
+++ src/components/SpeedList.tsx
@@ -21,11 +21,11 @@ const SPEEDS = computed(() => (
   )).flatMap((name) => {
     const baseSettings = {
       name,
-      attributes: POKEMONS[name].types,
-      teraAttribute: POKEMONS[name].types[0],
-      effectiveness: computeDefaultEffectiveness(POKEMONS[name].types),
-      teraEffectiveness: computeDefaultEffectiveness([POKEMONS[name].types[0]]),
-      baseStats: POKEMONS[name].stats,
+      attributes: POKEMONS[name].attributes,
+      teraAttribute: POKEMONS[name].attributes[0],
+      effectiveness: computeDefaultEffectiveness(POKEMONS[name].attributes),
+      teraEffectiveness: computeDefaultEffectiveness([POKEMONS[name].attributes[0]]),
+      baseStats: POKEMONS[name].baseStats,
       iv: { h: 31, a: 31, b: 31, c: 31, d: 31, s: 31 } as Values,
       ev: { h: 0, a: 0, b: 0, c: 0, d: 0, s: 0 } as Values,
       n: { a: 1, b: 1, c: 1, d: 1, s: 1 } as NValues,
